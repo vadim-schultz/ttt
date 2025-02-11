888932bcad6d622646d3097f275c195d2a4492a7
vadim-schultz <vadim.schultz@me.com>
2025-02-11 16:31:01 +0100

    Update schedule_test.py

--- schedule_test.py
+++ schedule_test.py
@@ -1,31 +1,5 @@
 from utils import schedule_from_players
-
-players = [
-    "Martin Danneberg",
-    "Alexandros Pollakis",
-    "Alexander Matthes",
-    "Thomas Horn",
-    "Dhanush Shankar",
-    "Joerg Reins",
-    "Tom Hößler",
-    "Khalil Ben Maaoui",
-    "Tobias Kadur",
-    "Frank Poegel",
-    "Martin Weitz",
-    "Georg Lauterbach",
-    "Uwe Proß",
-    "Michael Grieger",
-    "Marcel Valcan",
-    "Falk Hofmann",
-    "Sarmad Wahab",
-    "Kai Rücknagel",
-    "Udo",
-    "Süha Alp Azizoglu",
-    "Paul Kühne",
-    "Florian Eckardt",
-    "Susantha Ranatunga",
-    "Christian Siebke",
-]
+from env import players
 
 schedule = schedule_from_players(players)
 
